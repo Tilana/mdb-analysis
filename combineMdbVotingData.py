d523b75fa6f522fa575d3b42283d804686742a96
tilana <nataliewidmann@gmx.de>
2017-09-17 13:30:37 +0200

    format change: list for each voting the MdBs with their corresponding vote

--- combineMdbVotingData.py
+++ combineMdbVotingData.py
@@ -3,11 +3,12 @@ import pandas as pd
 import pdb
 
 PATH = '../data/'
-mdbFile = PATH + 'mdb-20170821.json'
+mdbFile = PATH + 'MdB.json'
 voteFile = PATH + 'mdbAbstimmungen.csv'
+voteDetailsFile = PATH + 'cleanAbstimmungen.csv'
 
 STUFEN = ['Stufe %i' % num for num in range(1,11)]
-KEYS= ['gender', 'state', 'academic_prefix', 'honorific_prefix', 'religion'] + STUFEN 
+KEYS= ['gender', 'state', 'academic_prefix', 'honorific_prefix', 'religion'] + STUFEN
 
 
 def loadJson(path):
@@ -27,17 +28,17 @@ def setData(data, ind, person):
                 value = len(value)
         except:
             value = '-'
-        data.loc[ind, key] = value 
+        data.loc[ind, key] = value
 
 
-def isMatch(name, person, data, field='Name'):
+def isMatch(name, person, data, field='name'):
     matches = data[data[field]==name.encode('utf8')]
     numberMatches = len(matches)
     if numberMatches == 1:
         return (True, matches.index[0])
     elif numberMatches > 1:
         firstName = person['given_name'].encode('utf8')
-        fullnameMatch = matches[matches['Vorname']==firstName]
+        fullnameMatch = matches[matches['first_name']==firstName]
         if len(fullnameMatch) == 1:
             return (True, fullnameMatch.index[0])
         else:
@@ -50,26 +51,55 @@ def combineMdbVotingData():
     personData= loadJson(mdbFile)
     persons = personData['persons']
 
-    data = loadCSV(voteFile)
+    voteData = loadCSV(voteFile)
+    voteDetails = loadCSV(voteDetailsFile)
+    voteDetails.drop_duplicates(inplace=True)
     fails = []
 
     for person in persons:
         name = person['family_name']
-        (exists, index)  = isMatch(name, person, data)
+        (exists, index)  = isMatch(name, person, voteData)
         if exists:
-            setData(data, index, person)
+            setData(voteData, index, person)
         else:
             print 'WARNING: %s  %s not found' % (person['given_name'], person['family_name'])
             fails.append(person)
 
     print '---------'
-    print 'Number of voting data: %i' % len(data)
+    print 'Number of voting data: %i' % len(voteData)
     print 'Number of MDb data: % i' % len(persons)
     print '   - Number of name mismatches: %i' % len(fails)
 
-    data = data.dropna(subset = ['Stufe 1', 'Stufe 10'])
-    print 'Number of combined data: %i' % len(data)
+    voteData = voteData.dropna(subset = ['Stufe 1', 'Stufe 10'])
+    print 'Number of combined data: %i' % len(voteData)
 
+
+    print '--------'
+
+    voteTitles = [column for column in voteData.columns if 'Abstimmung:' in column]
+    columnNames = [(title, title.split('_')[0]) for title in voteTitles]
+    voteData = voteData.rename(columns = dict(columnNames))
+    voteData.reset_index(inplace=True)
+    voteTitles = [column for column in voteData.columns if 'Abstimmung:' in column]
+
+    data = []
+
+    for voteTitle in voteTitles:
+        details = voteDetails[voteDetails.title == voteTitle]
+        try:
+            details = details.append([details] * (len(voteData)-1))
+            details.reset_index(inplace=True)
+
+            combinedData = pd.concat([details, voteData], axis=1)
+            combinedData['vote'] = voteData[voteTitle]
+            combinedData = combinedData.drop(voteTitles, axis=1)
+            data.append(combinedData)
+        except:
+            print 'Multiple voting data found'
+            print voteTitle
+            print ''
+
+    data = pd.concat(data)
     data.to_csv(PATH + 'combinedData.csv', encoding='utf8')
 
 
